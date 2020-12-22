89713fb407ffa63e6d0de19d44329629fbe37a66
oherba <44850388+oherba@users.noreply.github.com>
2020-12-22 23:05:43 +0100

    working on routers

--- ecommerce_project/src/App.js
+++ ecommerce_project/src/App.js
@@ -1,11 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './pages/homepage/homepage';
+// import HomePage from './pages/homepage/homepage';
+import {Switch, Route} from 'react-router-dom'
 
+const HomePage = (props)=>{
+  console.log(props)
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  )
+  }
+
+  const TopicList = (props)=>{
+    console.log(props)
+    return (
+      <div>
+        <h1>TopicList</h1>
+      </div>
+    )
+    }
+
+    const TopicDetail = (props)=>{
+      console.log(props)
+      return (
+        <div>
+          <h1>TopicDetail: {props.match.params.topicId}</h1>
+        </div>
+      )
+      }
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+    <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/topics' component={TopicList}/>
+      <Route path='/topics/:topicId' component={TopicDetail}/>
+    </Switch>
+   
     </div>
   );
 }
