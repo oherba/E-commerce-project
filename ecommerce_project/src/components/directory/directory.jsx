746c5d33c0844b845c2b1c06d2151dfe15cae766
oherba <44850388+oherba@users.noreply.github.com>
2020-12-22 15:58:49 +0100

    broken our homr into smaller components , finished styling our menu-item component

---
+++ ecommerce_project/src/components/directory/directory.jsx
@@ -0,0 +1,53 @@
+// import {Component } from 'react'
+import React from 'react'
+import MenuItem from '../menu-item/menu-item.jsx'
+import './directory.scss'
+
+class Directory extends React.Component {
+    constructor(){
+        super();
+        this.state ={
+            sections:[
+                {
+                    title: 'Phones',
+                    imageUrl: 'https://www.honorbuy.com/img/cms/1_33.jpg' ,
+                    id:1
+                },
+                {
+                    title: 'Cables',
+                    imageUrl: 'https://ae01.alicdn.com/kf/Hc741cd120f6f4a5880bbac0e2bb2bacep/Vothoon-2-4A-Magnetic-Micro-USB-Type-C-Cable-For-iphone-11-Pro-Xs-Samsung-S10.jpg_Q90.jpg' ,
+                    id:1
+                },
+                {
+                    title: 'Earphones',
+                    imageUrl: 'https://5.imimg.com/data5/DY/SN/IO/ANDROID-85067233/product-jpeg-500x500.jpeg' ,
+                    id:1
+                },
+                {
+                    title: 'Charger adapter',
+                    imageUrl: 'https://www.mytrendyphone.eu/images/TOPK-B244Q-Universal-QC3-0-Charger-28W-2xUSB-White-13072020-01A-p.jpg' ,
+                    size: 'large',
+                    id:1
+                },
+                {
+                    title: 'Usb',
+                    imageUrl: 'https://ae01.alicdn.com/kf/H77083d8b373f44fbb6812509a3d29af3K/JASTER-high-speed-USB-flash-drive-OTG-pen-drive-128gb-64gb-Usb-stick-32gb-256gb-Pendrive.jpg' ,
+                    size: 'large',
+                    id:1
+                }
+            ]
+        }
+    }
+
+    render(){
+        return(
+            <div className='directory-menu'>
+                {this.state.sections.map(({title, imageUrl, id, size}) =>(
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                ))}
+            </div>
+        )   
+    }
+}
+
+export default Directory
---
+++ ecommerce_project/src/pages/homepage/homepage.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import  './homePage.scss'
+import Directory from '../../components/directory/directory.jsx';
+
+
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory/>
+        {/* <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> CATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> Jackets</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'> Womens</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>Men</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+
+        </div> */}
+
+    </div>
+
+)
+
+export default HomePage
+// export default function homepage() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
